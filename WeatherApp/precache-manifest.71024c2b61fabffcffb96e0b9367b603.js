7ed8cbeba8a26fe9d40b9f962c7dc91b9b7e3af7
david-toker <54470976+david-toker@users.noreply.github.com>
2019-10-26 23:27:51 +0300

    Add files via upload

---
+++ WeatherApp/precache-manifest.71024c2b61fabffcffb96e0b9367b603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038f461f9193cd435700bf117fc2d9b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a17ae3ce116e1574abc6",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d5c1e10b0f9e72cf3ac9",
+    "url": "/static/js/2.60d7322c.chunk.js"
+  },
+  {
+    "revision": "a17ae3ce116e1574abc6",
+    "url": "/static/js/main.a6a244b9.chunk.js"
+  },
+  {
+    "revision": "2fdc80f7efa908c51768",
+    "url": "/static/js/runtime-main.0c4415d3.js"
+  }
+]);
